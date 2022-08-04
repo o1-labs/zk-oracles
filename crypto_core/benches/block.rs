9a1ce1cef27566db105aa9cef7c294341227fc6b
Xiang Xie <xiexiangiscas@gmail.com>
2022-08-04 12:26:39 +0800

    add bench on block and u128 operations

--- crypto_core/benches/block.rs
+++ crypto_core/benches/block.rs
@@ -35,6 +35,39 @@ fn bench_xor(c: &mut Criterion) {
     });
 }
 
+fn bench_u128_xor(c: &mut Criterion){
+    c.bench_function("U128::xor", |b|{
+        let x = rand::random::<u128>();
+        let y = rand::random::<u128>();
+        b.iter(||{
+            let z = x ^ y;
+            criterion::black_box(z)
+        });
+    });
+}
+
+fn bench_and(c: &mut Criterion) {
+    c.bench_function("Block::and", |b| {
+        let x = rand::random::<Block>();
+        let y = rand::random::<Block>();
+        b.iter(|| {
+            let z = x & y;
+            criterion::black_box(z)
+        });
+    });
+}
+
+fn bench_u128_and(c: &mut Criterion){
+    c.bench_function("U128::and", |b|{
+        let x = rand::random::<u128>();
+        let y = rand::random::<u128>();
+        b.iter(||{
+            let z = x & y;
+            criterion::black_box(z)
+        });
+    });
+}
+
 fn bench_default(c: &mut Criterion) {
     c.bench_function("Block::default", |b| {
         b.iter(|| {
@@ -47,6 +80,6 @@ fn bench_default(c: &mut Criterion) {
 criterion_group! {
     name = block;
     config = Criterion::default().warm_up_time(Duration::from_millis(100));
-    targets = bench_clmul, bench_rand, bench_xor, bench_default
+    targets = bench_clmul, bench_rand, bench_xor, bench_u128_xor, bench_and, bench_u128_and, bench_default
 }
 criterion_main!(block);
